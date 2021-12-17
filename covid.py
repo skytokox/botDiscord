46be4ed2d8e5786562eb2774b0047806c872a857
sky <50047674+skytokox@users.noreply.github.com>
2021-12-17 01:00:30 +0100

    Update discord.log

--- covid.py
+++ covid.py
@@ -190,8 +190,8 @@ class CovidData(commands.Cog):
         with open(f'./covid/dane_powiat_{date.strftime("%d.%m.%Y")}.csv',
                   mode='r', encoding='ISO-8859-1') as file:
             reader = csv.reader(file, delimiter=";")
-            print(reader)
             for row in reader:
+                print(row)
                 if "Cały kraj" == row[1]:
                     ilosc_zakazen = int(row[2])
                     ilosc_zakazen_100k = round(float(row[3]) * 10, 1)
